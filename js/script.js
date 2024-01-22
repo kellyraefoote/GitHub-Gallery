163187e0d9db80a17bc50f6ef38ea68abc26f743
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2024-01-22 13:24:24 -0800

    added list of repos

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 //This targets the div where profile information will appear//
 const overview = document.querySelector(".overview");
 const username = "kellyraefoote";
+const repoList = document.querySelector(".repo-list");
 
 const getData = async function () {
     const userInfo = await fetch(
         `https://api.github.com/users/${username}`);
         const data = await userInfo.json();
-       console.log(data);
+       //console.log(data);
         displayData(data);
 };
 
@@ -29,4 +30,24 @@ const displayData = function(data) {
      </div> 
     `;
     overview.append(div);
+    gitRepos();
+};
+
+const gitRepos = async function() {
+    const fetchRepos = await fetch(
+        //fetches the data, sorts it by most recently updated and limits to 100 per page//
+        `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+    )
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+}
+
+
+const displayRepos = function(repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 }
